b59f713dfcfba900ec9be5b2a4e3761083ddd58a
Zhi Yong Tan <yongish@gmail.com>
2024-03-13 17:58:55 -0700

    default setup. need to update routes.

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,9 +14,10 @@ const firebaseConfig = {
   storageBucket: "tingxie-admin.appspot.com",
   messagingSenderId: "1044706043973",
   appId: "1:1044706043973:web:417993f093300343af9b2c",
-  measurementId: "G-9HFPD8BB23"
+  measurementId: "G-9HFPD8BB23",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 const analytics = getAnalytics(app);
